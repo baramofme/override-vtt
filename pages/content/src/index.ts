4bfb360f7f78a1af5f8f868480806432d32364b1
JihoonYi <baramofme@gmail.com>
2025-04-08 00:04:28 +0900

    URL 모니터링 및 자막 번역 초기화를 구현

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -2,5 +2,50 @@ import { sampleFunction } from '@src/sampleFunction';
 
 console.log('content script loaded');
 
-// Shows how to call a function defined in another module
-sampleFunction();
+let lastUrl = '';
+//@Todo background script 와 중복. 통합 필요.
+const destinationHref = 'learn.deeplearning.ai/courses/';
+
+init();
+
+function init() {
+  checkUrlAndExecute();
+  registerEvents();
+  // sampleFunction();
+}
+
+function registerEvents() {
+  // @Todo (예: history.replaceState 사용)도 처리해야 하는지 고려해야 합니다.
+  window.addEventListener('popstate', checkUrlAndExecute);
+}
+
+/**
+ * Translates the caption on the page.
+ * This function prevents multiple calls by using a flag `isOn`.
+ * It sets `isOn` to true to indicate that translation has started and returns immediately if it's already true.
+ */
+function translateCaption() {
+  console.log('자막번역 시작');
+}
+
+function checkUrlAndExecute() {
+  const pageChanged = window.location.href !== lastUrl;
+  const onDestination = window.location.href.includes(destinationHref);
+  const offDestination = lastUrl.includes(destinationHref);
+
+  if (!lastUrl || pageChanged) {
+    // console.log('현재 위치:', window.location.href, '목적 href:', destinationHref);
+    if (onDestination) {
+      console.log('목적 페이지 진입. Executing code.');
+      translateCaption();
+    }
+    if (offDestination) {
+      console.log('목적 페이지 탈출. Executing code.');
+    }
+  }
+  updateLastUrl();
+}
+
+function updateLastUrl() {
+  lastUrl = window.location.href;
+}
